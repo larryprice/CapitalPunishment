a55615786caee3500d0299f289d0aad95e8e3203
Larry Price <larry.price.dev@gmail.com>
2012-09-09 15:27:31 -0400

    added US mode with images. Also added an image above the question and an icon for the page.

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 
 @@answer = Array.new
 @@question = ""
+@@question_statement = "What is the capital of"
 @@all_answers = Array.new
 
 get '/' do
@@ -16,7 +17,26 @@ post '/check_answer' do
   erb :CountriesAndCapitals
 end
 
+post '/check_us_answer' do
+  reload(compare_with_correct_answer(params[:answer].to_s.strip), @data, @country_info, @game_type, :US)
+  erb :StatesAndCapitals
+end
+
+post '/switch_to_world_mode' do
+  reload(compare_with_correct_answer(params[:answer].to_s.strip), @data, @country_info, @game_type)
+  erb :CountriesAndCapitals
+end
+
+post '/switch_to_us_mode' do
+  reload(compare_with_correct_answer(params[:answer].to_s.strip), @data, @country_info, @game_type, :US)
+  erb :StatesAndCapitals
+end
+
 def compare_with_correct_answer(answer)
+  if @@answer.empty? || @@question.empty?
+    return ""
+  end
+
   case answer
   when "first"
     selected_answer = @@all_answers[0]
@@ -29,38 +49,49 @@ def compare_with_correct_answer(answer)
   when "fifth"
     selected_answer = @@all_answers[4]
   else
-    return "No answer selected. Not sure how you did that, but bully for you."
+    return ""
   end
 
   if @@answer.include?(selected_answer)
     return 'Correct! The capital of ' + @@question.to_s + ' is ' + selected_answer.to_s + '.'
   else
-    return 'False. The capital of ' + @@question.to_s + ' is certainly not ' + selected_answer.to_s + '. Try harder'
+    return 'False. The capital of ' + @@question.to_s + ' is certainly not ' + selected_answer.to_s + '. Try harder.'
   end
 end
 
-def reload(result = nil, old_data = nil, old_info = nil, game_type = nil)
+def reload(result = nil, old_data = nil, old_info = nil, game_type = nil, game_mode = nil)
   @game_type = :CAPITALS
-  if old_data.nil?
-    data = get_data
-  else
-    data = old_data
-  end
 
-  if data.nil?
-    @error = "Can't find Wikipedia's List Of National Capitals. :("
-  else
-    if old_info.nil?
-      country_info = check_details(data)
+  if game_mode.nil? || game_mode == :WORLD
+    if old_data.nil?
+      data = get_world_data
     else
-      country_info = old_info
+      data = old_data
+    end
+
+    if data.nil?
+      @error = "Can't find Wikipedia's List Of Countries and Capitals. :("
+    else
+      if old_info.nil?
+        info = check_details(data)
+      else
+        info = old_info
+      end
+    end
+  else
+    # TODO: US Mode
+    data = get_state_data
+    if data.nil?
+      @error = "Can't find Wikipedia's List Of States and Capitals. :("
+    else
+      info = check_state_details(data)
     end
   end
 
-  get_new_question(country_info)
+  get_new_question(info)
 
   @data = data
-  @country_info = country_info
+  @country_info = info
 
   @result = result
 end
@@ -69,10 +100,36 @@ def check_details(data)
   details = get_details(data)
 
   if details.nil?
-    @error = "Apparently Wikipedia has changed their table format. :("
+    @error = 
+      "Apparently Wikipedia has changed their table format on the Countries page. :("
   end
   
   details
+end
+
+def check_state_details(data)
+  details = get_state_details(data)
+
+  if details.nil?
+    @error = 
+      "Apparently Wikipedia has changed their table format on the US page. :("
+  end
+
+  details
+end
+
+def get_state_details(data)
+  details = data.xpath("//table[@class='wikitable sortable']/tr").map do |row|
+
+    country = row.at_xpath('td[1]/a/text()').to_s.strip
+    next if country.nil?
+    capital = Array.new
+    capital << row.at_xpath('td[4]/a/text()').to_s.strip
+    { country => capital }
+  end
+  details = details.select { |detail| !detail.nil? }
+
+  p details
 end
 
 def get_details(data)
@@ -81,34 +138,32 @@ def get_details(data)
     country = row.at_xpath('td[2]/b/a/text()').to_s.strip
     if country.empty?
       country = row.at_xpath('td[2]/a/text()').to_s.strip
-      if country.empty?
-        next
-      end
+      next if country.empty?
     end
     if country != 'Tonga'
       { country => get_capital(row) }
     else
-      { country => 'Nuku\'alofa' }
+      { country => 'Nukuʻalofa' }
     end
   end
-
-  p details
 
   details = details.select { |detail| !detail.nil? }
 end
 
 def get_capital(row)
   capitals = Array.new
-
-  row.xpath('td[1]/a/text()').each do |cap|
-    capitals << cap.to_s.strip
-  end
+  row.xpath('td[1]/a/text()').each { |cap| capitals << cap.to_s.strip }
 
   capitals
 end
 
-def get_data
+def get_world_data
   url = "http://en.wikipedia.org/wiki/List_of_national_capitals"
+  Nokogiri::HTML(open(url))
+end
+
+def get_state_data
+  url = "http://en.wikipedia.org/wiki/List_of_capitals_in_the_United_States"
   Nokogiri::HTML(open(url))
 end
 
@@ -129,6 +184,7 @@ def get_new_question(country_info)
   else
     @@question = country_info[index].first[1]
     @@answer = country_info[index].first[0]
+    generate_answers_for_states @@answer, country_info
   end
 end
 
@@ -148,10 +204,16 @@ def generate_answers_for_capitals(right_answer, all_answers)
   @@all_answers = answers.sort_by {rand}
 end
 
+def generate_answers_for_states(right_answer, all_answers)
+  @@all_answers = Array.new 5
+end
+
 def toggle_game_type
   if @game_type == :CAPITALS
     @game_type = :COUNTRIES
+    @@question_statement = "What is the capital of"
   else
     @game_type = :CAPITALS
+    @@question_statement = "Where is"
   end
 end
