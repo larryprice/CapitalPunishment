5bca83c34e1190af4c2466ec4792dfd5d04fdf75
Larry Price <larry.price.dev@gmail.com>
2012-09-09 20:35:50 -0400

    Massive refactoring to make the two different game varieties their own classes with singleton instances

--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ require 'sinatra'
 require 'nokogiri'
 
 require 'open-uri'
+require 'singleton'
 
 @@answer = Array.new
 @@question = ""
@@ -13,253 +14,246 @@ require 'open-uri'
 @@game_type = :CAPITALS
 
 get '/' do
-  reload
   erb :CountriesAndCapitals
 end
 
 post '/check_answer' do
-  reload(compare_with_correct_answer(params[:answer].to_s.strip), @data, @country_info)
+  CountriesAndCapitals.instance.check_answer_and_get_new_question(params[:answer].to_s.strip)
   erb :CountriesAndCapitals
 end
 
 post '/check_us_answer' do
-  reload(compare_with_correct_answer(params[:answer].to_s.strip), @data, @country_info, :US)
+  StatesAndCapitals.instance.check_answer_and_get_new_question(params[:answer].to_s.strip)
   erb :StatesAndCapitals
 end
 
 post '/switch_to_world_mode' do
-  reload(compare_with_correct_answer(params[:answer].to_s.strip), nil, nil)
+  CountriesAndCapitals.instance.reset_result
   erb :CountriesAndCapitals
 end
 
 post '/switch_to_us_mode' do
-  reload(compare_with_correct_answer(params[:answer].to_s.strip), nil, nil, :US)
+  StatesAndCapitals.instance.reset_result
   erb :StatesAndCapitals
 end
 
 post '/toggle_mode' do
-  toggle_game_type
-  reload(compare_with_correct_answer(params[:answer].to_s.strip), @data, @country_info)
+  CountriesAndCapitals.instance.toggle_game_type_and_get_new_question
   erb :CountriesAndCapitals
 end
 
 post '/toggle_mode_us' do
-  toggle_game_type
-  reload(compare_with_correct_answer(params[:answer].to_s.strip), @data, @country_info, :US)
+  StatesAndCapitals.instance.toggle_game_type_and_get_new_question
   erb :StatesAndCapitals
 end
 
-def compare_with_correct_answer(answer)
-  if @@answer.empty? || @@question.empty?
-    return ""
+class CountriesAndCapitalsBase
+  def get_data
+    Nokogiri::HTML(open(@url))
   end
 
-  case answer
-  when "first"
-    selected_answer = @@all_answers[0]
-  when "second"
-    selected_answer = @@all_answers[1]
-  when "third"
-    selected_answer = @@all_answers[2]
-  when "fourth"
-    selected_answer = @@all_answers[3]
-  when "fifth"
-    selected_answer = @@all_answers[4]
-  else
-    return ""
-  end
-
-  if @@answer.include?(selected_answer)
-    return 'Correct! The capital of ' + @@question.to_s + ' is ' + selected_answer.to_s + '.'
-  else
-    return 'False. The capital of ' + @@question.to_s + ' is certainly not ' + selected_answer.to_s + '.'
-  end
-end
-
-def reload(result = nil, old_data = nil, old_info = nil, game_mode = nil)
-  if game_mode.nil? || game_mode == :WORLD
-    if old_data.nil?
-      data = get_world_data
+  def get_new_question
+    index = Random.rand(@info.count)
+    if @game_type == :CAPITALS
+      @question = @info[index].first[0]
+      @answer = @info[index].first[1]
+      generate_answers_for_capitals
     else
-      data = old_data
-    end
-
-    if data.nil?
-      @error = "Can't find Wikipedia's List Of Countries and Capitals. :("
-    else
-      if old_info.nil?
-        info = check_details(data)
-      else
-        info = old_info
-      end
-    end
-  else
-    if old_data.nil?
-      data = get_state_data
-    else
-      data = old_data
-    end
-    if data.nil?
-      @error = "Can't find Wikipedia's List Of States and Capitals. :("
-    else
-      if old_info.nil?
-        info = check_state_details(data)
-      else
-        info = old_info
-      end
-    end
-  end
-
-  get_new_question(info)
-
-  @data = data
-  @country_info = info
-
-  @result = result
-end
-
-def check_details(data)
-  details = get_details(data)
-
-  if details.nil?
-    @error = 
-      "Apparently Wikipedia has changed their table format on the Countries page. :("
-  end
-  
-  details
-end
-
-def check_state_details(data)
-  details = get_state_details(data)
-
-  if details.nil?
-    @error = 
-      "Apparently Wikipedia has changed their table format on the US page. :("
-  end
-
-  details
-end
-
-def get_state_details(data)
-  details = data.xpath("//table[@class='wikitable sortable']/tr").map do |row|
-
-    country = row.at_xpath('td[1]/a/text()').to_s.strip
-    next if country.nil? || country.empty?
-    capital = Array.new
-    capital << row.at_xpath('td[4]/a/text()').to_s.strip
-    { country => capital }
-  end
-  details = details.select { |detail| !detail.nil? }
-end
-
-def get_details(data)
-  details = data.xpath("//table[@class='wikitable sortable']/tr").map do |row|
-
-    country = row.at_xpath('td[2]/b/a/text()').to_s.strip
-    if country.nil? || country.empty?
-      country = row.at_xpath('td[2]/a/text()').to_s.strip
-      next if country.nil? || country.empty?
-    end
-    if country != 'Tonga'
-      { country => get_capital(row) }
-    else
-      { country => 'Nuku\'alofa' }
+      possible_questions = @info[index].first[1]
+      @question = possible_questions[Random.rand(possible_questions.count)]
+      @answer = @info[index].first[0]
+      generate_answers_for_states
     end
   end
   
-  details = details.select { |detail| !detail.nil? }
-end
+  def generate_answers_for_capitals
+    answers = Array.new
+    answers << @answer[Random.rand(@answer.count)]
+    filtered_answers = @info.delete_if { |a, b| b == @answer }
 
-def get_capital(row)
-  capitals = Array.new
-  row.xpath('td[1]/a/text()').each { |cap| capitals << cap.to_s.strip }
-
-  capitals
-end
-
-def get_world_data
-  url = "http://en.wikipedia.org/wiki/List_of_national_capitals"
-  Nokogiri::HTML(open(url))
-end
-
-def get_state_data
-  url = "http://en.wikipedia.org/wiki/List_of_capitals_in_the_United_States"
-  Nokogiri::HTML(open(url))
-end
-
-def print_output
-  if @error.nil?
-    puts @details
-  else
-    puts @error
-  end
-end
-
-def get_new_question(country_info)
-  index = Random.rand(country_info.count)
-  if @@game_type == :CAPITALS
-    @@question = country_info[index].first[0]
-    @@answer = country_info[index].first[1]
-    generate_answers_for_capitals @@answer, country_info
-  else
-    possible_questions = country_info[index].first[1]
-    @@question = possible_questions[Random.rand(possible_questions.count)]
-    @@answer = country_info[index].first[0]
-    generate_answers_for_states @@answer, country_info
-  end
-end
-
-def generate_answers_for_capitals(right_answer, all_answers)
-  answers = Array.new
-  answers << right_answer[Random.rand(right_answer.count)]
-  filtered_answers = all_answers.delete_if { |a, b| b == right_answer }
-
-  while answers.count < 5
+    while answers.count < 5
       wrong_answer = filtered_answers[Random.rand(filtered_answers.count)]
       wrong_answer_caps = wrong_answer.first[1]
       answer_to_add = wrong_answer_caps[Random.rand(wrong_answer_caps.count)]
       if !answers.include?(answer_to_add)
         answers << answer_to_add
       end
+    end
+
+    @all_answers = answers.sort_by {rand}
   end
 
-  @@all_answers = answers.sort_by {rand}
-end
+  def generate_answers_for_states
+    answers = Array.new
+    answers << @answer
+    filtered_answers = @info.delete_if { |a, b| a == @answer }
 
-def generate_answers_for_states(right_answer, all_answers)
-  answers = Array.new
-  answers << right_answer
-  filtered_answers = all_answers.delete_if { |a, b| a == right_answer }
+    while answers.count < 5
+      wrong_answer = filtered_answers[Random.rand(filtered_answers.count)]
+      wrong_answer_state = wrong_answer.first[0]
+      if !answers.include?(wrong_answer_state)
+        answers << wrong_answer_state
+      end
+    end
 
-  while answers.count < 5
-    wrong_answer = filtered_answers[Random.rand(filtered_answers.count)]
-    wrong_answer_state = wrong_answer.first[0]
-    if !answers.include?(wrong_answer_state)
-      answers << wrong_answer_state
+    @all_answers = answers.sort_by {rand}
+  end
+
+  def toggle_game_type
+    if !@game_type.nil? && @game_type == :CAPITALS
+      @game_type = :COUNTRIES
+      @question_statement = "Whose capital is"
+    else
+      @game_type = :CAPITALS
+      @question_statement = "What is the capital of"
     end
   end
 
-  @@all_answers = answers.sort_by {rand}
-end
+  def toggle_game_type_and_get_new_question
+    toggle_game_type
+    get_new_question
+    reset_result
+  end
 
-def toggle_game_type
-  if @@game_type == :CAPITALS
-    @@game_type = :COUNTRIES
-    @@question_statement = "Whose capital is"
-  else
-    @@game_type = :CAPITALS
-    @@question_statement = "What is the capital of"
+  def check_answer_and_get_new_question(answer)
+    compare_with_correct_answer(answer)
+    get_new_question
+  end
+
+  def compare_with_correct_answer(answer)
+    if @answer.empty? || @question.empty?
+      return ""
+    end
+
+    case answer
+    when "first"
+      selected_answer = @all_answers[0]
+    when "second"
+      selected_answer = @all_answers[1]
+    when "third"
+      selected_answer = @all_answers[2]
+    when "fourth"
+      selected_answer = @all_answers[3]
+    when "fifth"
+      selected_answer = @all_answers[4]
+    else
+      return ""
+    end
+
+    if @answer.include?(selected_answer)
+      @result = 'Correct! The capital of ' + @question.to_s \
+        + ' is ' + selected_answer.to_s + '.'
+    else
+      @result = 'False. The capital of ' + @question.to_s \
+        + ' is certainly not ' + selected_answer.to_s + '.'
+    end
+  end
+
+  def reset_result
+    @result = ""
+  end
+
+  def get_all_answers
+    @all_answers = Array.new(5) if @all_answers.nil?
+    @all_answers
+  end
+
+  def get_answer
+    @answer
+  end
+
+  def get_question
+    @question
+  end
+
+  def get_question_statement
+    @question_statement
+  end
+
+  def get_result
+    @result = "" if @result.nil?
+    @result
   end
 end
 
-def get_question_statement
-  @@question_statement = "Whose capital is" if @@question_statement.nil?
+class CountriesAndCapitals < CountriesAndCapitalsBase
+  include Singleton
+  def initialize
+    toggle_game_type
+    @url = "http://en.wikipedia.org/wiki/List_of_national_capitals"
+    @data = get_data
+    @info = check_details(@data)
+    get_new_question
+  end
 
-  @@question_statement
+  def check_details(data)
+    details = get_details(data)
+
+    if details.nil?
+      @error =
+        "Apparently Wikipedia has changed their table format on the Countries page. :("
+    end
+
+    details
+  end
+
+  def get_details(data)
+    details = data.xpath("//table[@class='wikitable sortable']/tr").map do |row|
+
+      country = row.at_xpath('td[2]/b/a/text()').to_s.strip
+      if country.nil? || country.empty?
+        country = row.at_xpath('td[2]/a/text()').to_s.strip
+        next if country.nil? || country.empty?
+      end
+      if country != 'Tonga'
+        { country => get_capital(row) }
+      else
+        { country => ['Nuku\'alofa'] }
+      end
+    end
+
+    details = details.select { |detail| !detail.nil? }
+  end
+
+  def get_capital(row)
+    capitals = Array.new
+    row.xpath('td[1]/a/text()').each { |cap| capitals << cap.to_s.strip }
+
+    capitals
+  end
 end
 
-def get_question
-  return "" if @@question.nil?
+class StatesAndCapitals < CountriesAndCapitalsBase
+  include Singleton
+  def initialize
+    toggle_game_type
+    @url = "http://en.wikipedia.org/wiki/List_of_capitals_in_the_United_States"
+    @data = get_data
+    @info = check_details(@data)
+    get_new_question
+  end
 
-  @@question
+  def check_details(data)
+    details = get_state_details(data)
+
+    if details.nil?
+      @error =
+        "Apparently Wikipedia has changed their table format on the US page. :("
+    end
+
+    details
+  end
+
+  def get_state_details(data)
+    details = data.xpath("//table[@class='wikitable sortable']/tr").map do |row|
+
+      country = row.at_xpath('td[1]/a/text()').to_s.strip
+      next if country.nil? || country.empty?
+      capital = Array.new
+      capital << row.at_xpath('td[4]/a/text()').to_s.strip
+      { country => capital }
+    end
+    
+    details = details.select { |detail| !detail.nil? }
+  end
 end
